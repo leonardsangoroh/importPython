e4d6ffbf4fc60b244c07e4ff8279a5d47fa0567c
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-10 15:35:09 +0300

    Output no. of characters in a string

--- Day1/input.py
+++ Day1/input.py
@@ -3,3 +3,15 @@ input("What is your name?")
 
 #Receiving input
 print("Hello " + input('What is your name?'))
+
+#Challenge
+#Output no. of characters in a string input
+
+#solution 1
+name = input('What is your name? ')
+
+print(len(name))
+
+#solution 2
+
+print(len(input("What is your name?")))
