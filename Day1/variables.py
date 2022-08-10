181752c46b262bbe72a61e4e69a9e410178ea81e
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-10 18:27:55 +0300

    variables coding exercise

---
+++ Day1/variables.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+print(name)
+
+#coding exercise (switch input a & b)
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b 
+b = c
+
+print("a = " + a)
+print ("b = " + b)
