e6652949445a3291fca48c25522b2c1b2b25d388
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-11 23:33:08 +0300

    operators

---
+++ Day2/operators.py
@@ -0,0 +1,16 @@
+#addition
+4 + 4
+
+#subtraction
+4-4
+
+#multiplication
+4*4
+
+#division (result is always of float data type)
+4/4
+
+#exponential
+4 ** 4
+
+#if there are more than one operators
