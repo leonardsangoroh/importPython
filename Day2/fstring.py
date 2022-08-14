d986d849c95c70fb18d262f112d4c4a1f9940509
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-14 18:39:27 +0300

    fstrings

---
+++ Day2/fstring.py
@@ -0,0 +1,11 @@
+#f-string
+
+# why f-string?
+# because the print() function cannot accept 2 different data types as arguments
+
+score = 0
+height = 1.8
+isWinning = True
+
+#fstring
+print(f"My score is {score}, and my height is {height}")
