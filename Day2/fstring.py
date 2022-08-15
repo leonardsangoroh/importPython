3e2028589f75f53a41ca85b5eb7c57264a0cb605
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-15 13:28:58 +0300

    coding exercise

--- Day2/fstring.py
+++ Day2/fstring.py
@@ -9,3 +9,23 @@ isWinning = True
 
 #fstring
 print(f"My score is {score}, and my height is {height}")
+
+# coding exercise
+# program that tells us how many days, weeks, months we have left
+# Assuming we live until 90 years old
+
+ageLimit = 90
+oneYearDays = 365
+oneYearWeeks = 52
+oneYearMonths = 12
+
+age = input("What is your current age? ")
+
+ageInteger = int(age)
+
+remainingYears = ageLimit - ageInteger
+remainingDays = remainingYears * oneYearDays
+remainingWeeks = remainingYears * oneYearWeeks
+remainingMonths = remainingYears * oneYearMonths
+
+print(f"You have {remainingDays} days, {remainingWeeks} weeks, and {remainingMonths} months left")
