f391e9a2df26867d7a6ace647bf1fb88c9325f1f
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-16 23:11:19 +0300

    nested if statement

--- Day3/controlFlow.py
+++ Day3/controlFlow.py
@@ -1,16 +1,25 @@
-from unittest import result
-
+# check height of a person
+# if greater than 120, they can ride, else they cannot
+# also check age & determine how much they should pay
 
 print("Welcome to the Roalercoaster!")
 height = int(input("What is your height in cm?"))
 
 if height > 120:
     print("You can ride the roalercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5.00")
+    elif age <= 18:
+        print("Please pay $7.00")
+    else:
+        print("Please pay $12.00")
 else:
     print("Sorry, you have to grow taller before you can ride")
 
 # coding exercise
 # check whether the number is odd or even
+
 
 number = int(input("Which number do you want to check? "))
 
