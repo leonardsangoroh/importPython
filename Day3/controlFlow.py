2627c28a241b7943b2a68fed517a5511777afb0d
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-16 14:35:29 +0300

    coding exercise

--- Day3/controlFlow.py
+++ Day3/controlFlow.py
@@ -1,3 +1,6 @@
+from unittest import result
+
+
 print("Welcome to the Roalercoaster!")
 height = int(input("What is your height in cm?"))
 
@@ -5,4 +8,16 @@ if height > 120:
     print("You can ride the roalercoaster!")
 else:
     print("Sorry, you have to grow taller before you can ride")
-    
+
+# coding exercise
+# check whether the number is odd or even
+
+number = int(input("Which number do you want to check? "))
+
+result = number%2
+
+if result == 1:
+    print("odd")
+
+else:
+    print("even")
