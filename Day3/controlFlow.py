7561b0d0fa45dcfa16d1c567f82fc2b074683dc5
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-08-16 14:21:20 +0300

    control flow (if else)

---
+++ Day3/controlFlow.py
@@ -0,0 +1,8 @@
+print("Welcome to the Roalercoaster!")
+height = int(input("What is your height in cm?"))
+
+if height > 120:
+    print("You can ride the roalercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride")
+    
