4b92b6b59d5af260baa66c16da59965370776683
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-09-04 22:04:05 +0300

    back to python basics

---
+++ programizPythonIntro.py
@@ -0,0 +1,22 @@
+#Indentifiers - names given to variables, classes, methods etc
+language = 'Python'
+print(language)
+
+#####################Commenting################################
+
+#Single line comment
+
+#Multiple
+#Line
+#Comment
+
+"""
+Multiple line comment
+Used to declare multiple line strings but can be used for comments
+if not assigned to a variable
+"""
+
+#####################Variables################################
+
+#we use the assignment operator '=' to assign a value
+number = 10
