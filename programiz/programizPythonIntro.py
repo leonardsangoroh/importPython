82180087eed0a35074a458edc86aa6e58206701a
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-09-04 22:55:56 +0300

    Constants - python

---
+++ programiz/programizPythonIntro.py
@@ -0,0 +1,44 @@
+#Indentifiers - names given to variables, classes, methods etc
+language = 'Python'
+print(language)
+
+#####################Commenting################################
+
+#Single line comment
+
+#Multiple
+#Line
+#Comment
+
+"""
+Multiple line comment
+Used to declare multiple line strings but can be used for comments
+if not assigned to a variable
+"""
+
+#####################Variables################################
+
+#we use the assignment operator '=' to assign a value
+number = 10
+print(number)
+
+#changing the value of a variable in python
+number = 11
+print(number)
+
+#assigning multiple values to multiple variables
+a, b, c = 5, 3.2, 'Lee'
+
+#assign same value to multiple variables
+programmerOne = programmerTwo = 'Lee'
+
+#python constants
+
+#constant - a special type of variable who's value can't be changed
+
+#Constants are declared and assigned in a modle - a separate file
+#containing variables, functions ets which is imported to the main file
+
+import constant
+
+print(constant.PI)
