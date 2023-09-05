91e6c8928772e1ff79666fa22f90b1f8a22b72a6
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-09-06 01:38:51 +0300

    I/O - complete

--- programiz/programizPythonIntro.py
+++ programiz/programizPythonIntro.py
@@ -124,3 +124,94 @@ print('It is rainy today')
 print('New Year', 2023, 'See you soon!', sep= '. ')
 
 #output: New Year. 2023. See you soon!
+
+#python input
+num = input('Enter a number: ')
+
+#its data type is a string and not a number
+print(f'you entered {num}')
+print('Data type of num: ', type(num))
+
+#convert it into and integer
+num = int(input('Enter a number: '))
+
+#python operators
+
+#arithmetic operators
+# +, -, *, /, //(flooe division - no remainder), %, **(power)
+
+#assignment operators
+#used to assign values to variables
+
+#assign 5 to x
+x = 5
+y = 4
+
+#assign the sum of x and y to x
+x += y
+
+#comparision operators
+a = 5
+
+b = 2
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
+
+#logical operators
+#check whether an expression is true or false
+# and, or, not
+print((a > 2) and (b >= 6))
+
+#bitwise operators
+#they act on operands as if they were a string of binary digits
+
+
+#special operators
+#check whether two values are located on the same part of the memory
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+
+print(x1 is not y1)  # prints False
+
+print(x2 is y2)  # prints True
+
+print(x3 is y3)  # prints False
+
+#membership operators
+#used to test whether a value is found in a sequence
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+
+# check if '1' key is present in y
+print(1 in y)  # prints True
+
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+#####################namespaces################################
