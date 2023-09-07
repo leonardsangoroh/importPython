8597be582a47c582c2fdf37166e2ab5faf4e9077
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-09-07 23:24:58 +0300

    for loop - completed

--- programiz/pythonFlowControl2.py
+++ programiz/pythonFlowControl2.py
@@ -43,3 +43,40 @@ else:
     print('Number is negative')
 
 # Output: Number is positive
+
+########################################for loop############################################
+
+#loops - used to repeat a block of code
+languages = ['Swift','Python','SQL']
+
+for language in languages:
+    print(language)
+
+for x in 'Python':
+    print(x)
+
+##################################for loop with python range####################################
+values = range(4)
+
+for i in values:
+    print(i)
+
+#############################for loop without accessing items###############################
+for language in languages:
+    print('Hello')
+    print('Leonard')
+    #the output prints 'Hello' and 'Leonard' 3 times
+    #we did not necessarily print the items of the list
+
+#if the items of a sequence is not to be used, the loop can be written as follows
+for _ in language:
+    print("Hello Leonard")
+
+#############################for loop wit else###############################
+#the else part is executed after the loop iterates through every item of a sequence
+digits = [0,3,6]
+
+for i in digits:
+    print(i)
+else:
+    print('No items left')
