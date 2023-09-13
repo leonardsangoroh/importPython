8402d4bba00be61ea647f634d98420bbc568549d
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-09-13 19:48:41 +0300

    python recursion & lambda functions

---
+++ programiz/pythonFunctions3.py
@@ -0,0 +1,85 @@
+##########################################functions###########################################
+#there are standard library functions (built-in functions) & user-defined functions
+def greet():
+    print("Semaje Sangoroh")
+
+greet()
+
+#function with 2 arguments
+def add_numbers(num1,num2):
+    sum = num1+num2
+    return sum
+
+answer = add_numbers(9,92)
+print(answer)
+
+#python library function
+
+import math
+
+#squareRoot
+squareRoot = math.sqrt(4)
+
+#power
+power = pow(2,3)
+
+#function argument with default values
+def summation (a=7,b=3):
+    sum = a + b
+    print(sum)
+
+#ways of calling summation()
+summation(4,48)
+
+summation(48)
+
+summation()
+
+#python keyword argument
+def displayNames(fName, lName):
+    print(fName, ' ', lName)
+
+#function call - the order of arguments passed doesn't matter
+displayNames(lName='Sangoroh',fName='Lee')
+
+#function with arbitrary arguments
+def findSum(*numbers):
+
+    result = 0
+
+    for number in numbers:
+        result = result + number
+    print(result)
+
+# function call with 3 arguments
+findSum(1, 2, 3)
+
+# function call with 2 arguments
+findSum(4, 9)
+
+####################################python recursion###################################
+#recursion is the process of defining something in terms of itself
+
+#every recursive condition must have a base condition
+
+def factorial(X):
+
+    if X == 1:
+        return 1
+    else:
+        return(X * factorial(X-1))
+
+
+num = 3
+print("The factorial of ", num, "is ", factorial(num))
+
+###############################python lambda/ anonymous function##############################
+nisalimie = lambda : print("Niaje Sangoroh")
+
+#call the lambda function
+nisalimie()
+
+#lambda function with an argument
+nisalimieVizuri = lambda name : print("Semaje ", name)
+
+nisalimieVizuri('Lee Leonard Sangoroh')
