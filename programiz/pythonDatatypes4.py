8187d7468e4faf1817037f5c1d0958134e5e730c
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-09-19 21:37:44 +0300

    lists - complete

--- programiz/pythonDatatypes4.py
+++ programiz/pythonDatatypes4.py
@@ -62,3 +62,74 @@ print(math.sinh(1))
 print(math.factorial(6))
 
 ##################################python lists########################################
+#a list can store elements of different data types and also store duplicate elements
+
+listOne = [1, 'Lee', 'Lee']
+
+#accessing list elements
+print(listOne[2])
+
+#negative indexing
+#the index -1 refers to the last item, -2 refers to the second last item
+
+#slicing a list - getting a portion of a list
+print(listOne[0:2]) #get from index 0 to 1
+print(listOne[1:])  # from index 1 to the end
+print(listOne[:]) #print from beginning to end
+
+#add elements to a list
+
+# .append adds element to the end of the list
+numbers = [0,1,2,3]
+numbers.append(4)
+
+# .extend adds all items of an iterable
+moreNumbers = [34,55,68,22,82]
+
+numbers.extend(moreNumbers)
+
+# .insert adds at a specified index
+
+#insert 30 at index 1
+numbers.insert(1,30)
+
+
+#change list items
+numbers[4] = 33
+
+#remove items from a list
+
+#using del statement
+del moreNumbers[1]
+
+del moreNumbers[-1]
+
+del moreNumbers[0:2]
+
+#using remove
+listOne.remove('Lee')
+
+
+#iterating through a list
+languages = ['Python','Swift','MySQL']
+
+for language in languages:
+    print(language)
+
+#check element existence - return True or False
+print('English' in languages)
+
+#check length
+print(len(languages))
+
+
+#list comprehension - creating a list
+nambari = [n**2 for n in range(1,6)]
+print(nambari)
+
+#or
+
+nambariHalisi = []
+
+for n in range(1,6):
+    nambariHalisi.append(n**2)
