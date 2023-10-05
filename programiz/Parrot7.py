cb8f707363a8c282c59e6b03640138d8196a84f4
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-10-05 23:53:16 +0300

    python classes

---
+++ programiz/Parrot7.py
@@ -0,0 +1,13 @@
+class Parrot7:
+    #class attribute
+    name = ""
+    age = 0
+
+#create a parrot object
+parrotOne = Parrot7()
+parrotOne.name = "Leo"
+parrotOne.age = 3
+
+#access attributes
+print(f"{parrotOne.name} is {parrotOne.age} years old")
+
